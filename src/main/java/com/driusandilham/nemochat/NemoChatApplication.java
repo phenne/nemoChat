b078a8017a6e13f5453689eff5bb40df1af4a5c1
phenne <gufimy@mail.ru>
2018-02-06 00:27:55 +0300

    initial #2

---
+++ src/main/java/com/driusandilham/nemochat/NemoChatApplication.java
@@ -0,0 +1,12 @@
+package com.driusandilham.nemochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemoChatApplication.class, args);
+	}
+}
