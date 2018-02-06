f5c36743e150a386142a47f0203ea4b9d9223f44
ilhamkhabibullin <mailbox.ilham@gmail.com>
2018-02-06 19:31:59 +0300

    ***

--- webapp/src/components/App.jsx
+++ webapp/src/components/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component {
     render () {
         return (
             <div className="App">
-                AppJsxComponent
+                PullRequest
             </div>
         )
     }
