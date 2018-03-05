d313ec75ddc6c5efc5d2a441e7c7863664b5e1b9
phenne <gufimy@mail.ru>
2018-03-05 04:52:49 +0300

    Fix paths to html, js, css

--- src/main/java/com/driusandilham/nemochat/NemoChatApplication.java
+++ src/main/java/com/driusandilham/nemochat/NemoChatApplication.java
@@ -3,10 +3,20 @@ package com.driusandilham.nemochat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 @SpringBootApplication
 public class NemoChatApplication {
 
 	public static void main(String[] args) {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+            System.out.println(url.getFile());
+        }
 		SpringApplication.run(NemoChatApplication.class, args);
 	}
 }
