eebbc04cd52087981da0df78e225ea139b84df41
phenne <gufimy@mail.ru>
2018-03-16 21:46:06 +0300

    Mongo Config, username and password properties

--- src/main/java/com/driusandilham/nemochat/config/MongoConfig.java
+++ src/main/java/com/driusandilham/nemochat/config/MongoConfig.java
@@ -3,9 +3,12 @@ package com.driusandilham.nemochat.config;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -26,14 +29,26 @@ public class MongoConfig extends AbstractMongoConfiguration {
     @Value("${db.port}")
     private String port;
 
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
     @Override
     protected String getDatabaseName() {
         return databaseName;
     }
 
     @Override
-    public Mongo mongo() throws Exception {
+    public Mongo mongo() {
         return new MongoClient(host, Integer.parseInt(port));
+    }
+
+    @Override
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongo(), getDatabaseName(), new UserCredentials(username, password));
     }
 
     @Override
